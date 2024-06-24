0fd84405293d56e1734fef7ac0b2020bf964b933
yusuffenes <budaky4343@gmail.com>
2024-06-24 23:29:53 +0300

    th_u

--- cryptoApi.py
+++ cryptoApi.py
@@ -4,16 +4,16 @@ def get_crypto_price(symbol):
     params={
         'symbol': symbol
     }
-    rp = requests.get('https://api.binance.com/api/v3/ticker/price?', params=params)
-    return float(rp.json()['price'])
+    rq = requests.get('https://api.binance.com/api/v3/ticker/price?', params=params)
+    return float(rq.json()['price'])
 
 
 def get_crypto_info(symbol):
     params={
         'symbol': symbol
     }
-    rp = requests.get('https://api.binance.com/api/v3/ticker/24hr?', params=params)
-    return rp.json()
+    rq = requests.get('https://api.binance.com/api/v3/ticker/24hr?', params=params)
+    return rq.json()
 
 def get_coin_names():
     rp = requests.get('https://api.binance.com/api/v3/ticker/price')
